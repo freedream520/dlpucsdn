80a3fb9a7ca13588421636dd7ffec95ceca8525c
tcitry <tcitry@gmail.com>
2014-11-10 21:15:45 +0800

    big

--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 from django.shortcuts import render, render_to_response, RequestContext
 from account.models import department
 from django.http.response import HttpResponseRedirect
@@ -19,15 +19,15 @@ def create_topic(request, dn):
     if request.method == 'GET':
         return render_to_response('forum/create.html', {'user': request.user,
                                                         'dn': dn,
-                                                        'title':u'创建一个主题',
+                                                        'title': u'创建一个主题',
                                                         'department': d.cn},
                                   context_instance=RequestContext(request))
     elif request.method == 'POST':
         title = request.POST['title']
         content = request.POST['content']
         if not title:
-            messages.add_message(request,messages.WARNING,_(u'标题不能为空'))
-            return HttpResponseRedirect(reverse('create_topic',kwargs={'dn':dn}))
+            messages.add_message(request, messages.WARNING, _(u'标题不能为空'))
+            return HttpResponseRedirect(reverse('create_topic', kwargs={'dn': dn}))
         t = topic()
         t.department_name = d
         t.title = title
@@ -41,10 +41,10 @@ def forum_index(request, dn):
     d = department.objects.get(name=dn)
     t = topic()
     t.department_name = d
-    topics = topic.objects.filter(department_name=d,deleted = False)
+    topics = topic.objects.filter(department_name=d, deleted=False)
     return render_to_response('forum/index.html', {'dn': dn,
                                                    'user': request.user,
-                                                   'title':u'主题列表',
+                                                   'title': u'主题列表',
                                                    'topics': topics,
                                                    'department': d.cn},
                               context_instance=RequestContext(request))
@@ -53,7 +53,7 @@ def forum_index(request, dn):
 def topic_view(request, dn, topic_id):
     d = department.objects.get(name=dn)
     t = topic.objects.get(id=topic_id)
-    r = reply.objects.filter(topic=topic_id)
+    r = reply.objects.filter(topic=topic_id, deleted=False)
     t.click += 1
     t.save()
     return render_to_response('forum/topic.html', {'user': request.user,
@@ -61,7 +61,7 @@ def topic_view(request, dn, topic_id):
                                                    'department': d.cn,
                                                    'reply': r,
                                                    'title': t.title,
-                                                   'request':request,
+                                                   'request': request,
                                                    'topic_id': topic_id,
                                                    'topics': t},
                               context_instance=RequestContext(request))
@@ -76,19 +76,21 @@ def create_reply(request, dn, topic_id):
         r.topic = t
         r.content = request.POST['reply']
         r.save()
-        t.click -=1
+        t.click -= 1
         t.last_replied = datetime.now()
-        t.reply_count +=1
+        t.reply_count += 1
         t.save()
-        return HttpResponseRedirect(reverse('topic_view',kwargs={'topic_id':topic_id,
-                                                                 'dn':dn}))
+        return HttpResponseRedirect(reverse('topic_view', kwargs={'topic_id': topic_id,
+                                                                  'dn': dn}))
+
 
 def del_topic(request, dn, topic_id):
-    t = topic.objects.get(id = topic_id)
+    t = topic.objects.get(id=topic_id)
     if request.user == t.auth:
         t.deleted = True
         t.save()
-        messages.add_message(request,messages.WARNING,_(u'删除成功！'))
+        messages.add_message(request, messages.WARNING, _(u'删除成功！'))
     else:
-        messages.add_message(request,messages.WARNING,_(u'删除失败，你不是这个主题的作者！'))
-    return HttpResponseRedirect(reverse('forum_index',kwargs={'dn':dn}))
+        messages.add_message(request, messages.WARNING, _(u'删除失败，你不是这个主题的作者！'))
+    return HttpResponseRedirect(reverse('forum_index', kwargs={'dn': dn}))
+
